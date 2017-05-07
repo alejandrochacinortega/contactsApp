a9e8a2818efe3686a43257fa8bcb9786e772b62a
Omar Alejandro Chacin Ortega <omaralejandrochacinortega@Omars-MacBook-Pro.local>
2017-05-07 19:52:40 +0300

    add react-navigation

--- app/config/colors.js
+++ app/config/colors.js
@@ -2,5 +2,5 @@ export default {
   background: '#ffffff',
   primaryText: '#525252',
   subleText: '#9a9a9a',
-  rowUnderlay: 'rgba(154, 154, 154, 0.9'
+  rowUnderlay: 'rgba(154, 154, 154, 0.25)'
 }
