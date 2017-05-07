78ce880ea58b711df88633d8ded36edcf8e3a1c5
Omar Alejandro Chacin Ortega <omaralejandrochacinortega@Omars-MacBook-Pro.local>
2017-05-07 21:01:38 +0300

    pass data to details view

--- app/config/router.js
+++ app/config/router.js
@@ -2,7 +2,7 @@ import { StackNavigator } from 'react-navigation';
 
 import Contacts from '../screens/Contacts';
 import Details from '../screens/Details';
-
+import { capitalizeFirstLetter } from '../helpers/string';
 export const ContactsStack = StackNavigator({
   Contacts: {
     screen: Contacts,
@@ -12,8 +12,8 @@ export const ContactsStack = StackNavigator({
   },
   Details: {
     screen: Details,
-    navigationOptions: {
-      title: 'Details'
-    }
+    navigationOptions: ({ navigation }) => ({
+      headerTitle: `${capitalizeFirstLetter(navigation.state.params.name.first)} ${capitalizeFirstLetter(navigation.state.params.name.last)}`,
+    }),
   }
 });
